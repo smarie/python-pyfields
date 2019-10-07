7d720d98151e7769b49525f197107f683d6094ab
Sylvain MARIE <sylvain.marie@se.com>
2019-10-07 21:53:19 +0200

    Fixed stub file to match original file

--- pyfields/init_makers.pyi
+++ pyfields/init_makers.pyi
@@ -3,28 +3,14 @@ from typing import Union, Any, Callable, Iterable
 from pyfields.core import Field
 
 
-def inject_fields(*fields: Union[Field, Any],
-                  ):
-    ...
-
-
-class InjectedInitDescriptor(object):
-    ...
-
-
-class InjectedInitFieldsArg(object):
-    ...
-
-
-def create_injected_init(init_fun,
-                         fields: Iterable[Field]
-                         ):
-    ...
-
-
 def init_fields(*fields: Union[Field, Any],
                 init_args_before: bool = True
                 ):
+    ...
+
+
+def inject_fields(*fields: Union[Field, Any],
+                  ):
     ...
 
 
@@ -32,4 +18,21 @@ def make_init(*fields: Union[Field, Any],
               post_init_fun: Callable = None,
               post_init_args_before: bool = True
               ):
+    ...
+
+
+class InitDescriptor(object):
+    def init(self, obj): ...
+    ...
+
+
+class InjectedInitFieldsArg(object):
+    ...
+
+
+def create_init(fields: Iterable[Field],
+                user_init_fun: Callable[[...], Any] = None,
+                inject_fields: bool = False,
+                user_init_args_before: bool = True
+                ):
     ...
