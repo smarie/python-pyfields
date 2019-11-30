65971751791ae11c052faee054e5bd7e267d6d31
Sylvain MARIE <sylvain.marie@se.com>
2019-11-30 21:12:18 +0100

    Added test

---
+++ pyfields/tests/issues/test_issue_53.py
@@ -0,0 +1,24 @@
+#  Authors: Sylvain Marie <sylvain.marie@se.com>
+#
+#  Copyright (c) Schneider Electric Industries, 2019. All right reserved.
+
+from pyfields import field, init_fields, get_field
+
+
+def test_issue_53():
+
+    class A(object):
+        a = field(str, check_type=True)
+
+        @init_fields()
+        def __init__(self):
+            pass
+
+    class B(A):
+        b = field(str, check_type=True)
+
+        @init_fields()
+        def __init__(self):
+            super(B, self).__init__(a=self.a)
+
+    print(B('a', 'b'))
