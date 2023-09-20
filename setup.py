78963c69a8607e6c25943bcf7bf31c46b4744dfd
vivekkumar15 <kmr.vivek15@gmail.com>
2023-09-20 17:01:11 +0530

    setup 2

--- setup.py
+++ setup.py
@@ -14,13 +14,11 @@ See also:
 """
 from setuptools import setup
 
-
 # (1) check required versions (from https://medium.com/@daveshawley/safely-using-setup-cfg-for-metadata-1babbe54c108)
 import pkg_resources
 
 pkg_resources.require("setuptools>=39.2")
 pkg_resources.require("setuptools_scm")
-
 
 # (2) Generate download url using git version
 from setuptools_scm import get_version  # noqa: E402
@@ -28,12 +26,9 @@ from setuptools_scm import get_version  # noqa: E402
 URL = "https://github.com/smarie/python-pyfields"
 DOWNLOAD_URL = URL + "/tarball/" + get_version()
 
-
 # (3) Call setup() with as little args as possible
-#setup(
-#    download_url=DOWNLOAD_URL,
-#    use_scm_version={
-#        "write_to": "pyfields/_version.py"
-#    },  # we can't put `use_scm_version` in setup.cfg yet unfortunately
-#)
-
+setup(
+    download_url=DOWNLOAD_URL,
+    version="1.7.1.dev9+gc08cfb3",
+    # we can't put `use_scm_version` in setup.cfg yet unfortunately
+)
